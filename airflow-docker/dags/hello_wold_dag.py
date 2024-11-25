5cef327fa37ddd81c1c2765f482f205ad816a096
Steven <stevenliuthegreat@gmail.com>
2024-11-25 12:45:39 -0800

    kafka producer + consumers for transaction data

--- airflow-docker/dags/hello_wold_dag.py
+++ airflow-docker/dags/hello_wold_dag.py
@@ -11,7 +11,7 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 
 # Function to run your Python code
-def hello_world():
+def hello_airflow():
     print("Hello from Airflow!")
 
 # Define the DAG
@@ -20,8 +20,8 @@ dag = DAG('hello_airflow', description='A simple hello world DAG',
 
 # Define a task that will run the hello_world function
 task = PythonOperator(
-    task_id='hello_task',
-    python_callable=hello_world,
+    task_id='hello_airflow',
+    python_callable=hello_airflow,
     dag=dag,
 )
 
@@ -45,3 +45,14 @@ with DAG(
     )
 
 # This DAG does not include dependencies or scheduling, so it only has one task.
+
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+with DAG(
+    dag_id='trigger_parent_dag',
+    start_date=datetime(2024, 11, 1),
+    schedule_interval='@daily',
+) as parent_dag:
+    trigger_child_dag = TriggerDagRunOperator(
+        task_id='trigger_child_dag',
+        trigger_dag_id='hello_task',  # ID of the DAG to trigger
+    )
