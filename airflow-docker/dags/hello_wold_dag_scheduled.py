d853cf23c2b2fbb1f68171d1031583e469774ac6
Steven <stevenliuthegreat@gmail.com>
2024-11-27 15:15:05 -0800

    minimum scheduled_interval time is 1 min.

--- airflow-docker/dags/hello_wold_dag_scheduled.py
+++ airflow-docker/dags/hello_wold_dag_scheduled.py
@@ -5,12 +5,13 @@ from datetime import datetime
 # Define the Python function to be executed
 def hello_world():
     print("Hello, World! Schduled every 30 seconds")
-
+    print(f"Current Time: {datetime.now()}")
+    return "Example Returned Value"
 # Define the DAG (Directed Acyclic Graph)
 dag = DAG(
-    'example_hello_world',  # Name of the DAG
+    'hello_task_schedule_every_30s',  # Name of the DAG
     description='An example DAG to print Hello World every 30 seconds',
-    schedule_interval='*/30 * * * * *',  # Run every 30 seconds (Airflow cron expression)
+    schedule_interval='*/1 * * * *',  # Run every minute (this is the minimum with schedule_interval)
     start_date=datetime(2024, 1, 1),  # Start date of the DAG
     catchup=False,  # To prevent running past missed intervals
 )
