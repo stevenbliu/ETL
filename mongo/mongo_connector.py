282d49bae98b8aa5f190ce45f50f2a57f43194b3
Steven <stevenliuthegreat@gmail.com>
2024-12-09 00:09:36 -0800

    set up mongodb

--- mongo/mongo_connector.py
+++ mongo/mongo_connector.py
@@ -1,14 +1,14 @@
 from pymongo import MongoClient
 
 # Connect to MongoDB
-client = MongoClient('mongodb://admin:adminpassword@localhost:27017')
+client = MongoClient("mongodb://root:example@localhost:27017/admin")
 
 # Select the database and collection
 db = client.mydatabase
 collection = db.users
 
 # Insert a document
-document = {"name": "Dave", "age": 35}
+document = {"name": "Dave2", "age": 35}
 result = collection.insert_one(document)
 
 print(f"Inserted document with _id: {result.inserted_id}")
